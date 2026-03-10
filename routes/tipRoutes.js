dc5b7ebc800ddc450cfab960bba1c067f49bd481
NNC <155552629+NNCDEV25@users.noreply.github.com>
2026-03-10 17:11:01 +0530

    final code backend

--- routes/tipRoutes.js
+++ routes/tipRoutes.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const Tip = require("../models/Tip");
 
 router.get("/", async (req, res) => {
-  try {
-    const tip = await Tip.findOne().sort({ _id: -1 });
-    res.json(tip);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  const tips = await Tip.find().sort({ _id: -1 });
+  res.json(tips);
+});
+
+router.post("/", async (req, res) => {
+  const tip = new Tip(req.body);
+  await tip.save();
+  res.json(tip);
+});
+
+router.delete("/:id", async (req, res) => {
+  await Tip.findByIdAndDelete(req.params.id);
+  res.json({ message: "Tip deleted" });
 });
 
 module.exports = router;
